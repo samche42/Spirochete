e7bcdd8c0c144bb01412e0545b2d11f03a4dbceb
Sam Waterworth <60663840+samche42@users.noreply.github.com>
2024-01-03 07:43:42 -0500

    Create fetch_ncbi_assemblies.sh

---
+++ fetch_ncbi_assemblies.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --partition=
+#SBATCH --time=14-00:00:00
+#SBATCH -N 1 # Nodes
+#SBATCH -n 1 # Tasks
+#SBATCH --cpus-per-task=10
+#SBATCH --error=ncbi_download.%J.err
+#SBATCH --output=ncbi_download.%J.out
+
+cat accessions.txt | while read -r acc ; do
+    esearch -db assembly -query $acc </dev/null \
+        | esummary \
+        | xtract -pattern DocumentSummary -element FtpPath_GenBank \
+        | while read -r url ; do
+            fname=$(echo $url | grep -o 'GCA_.*' | sed 's/$/_genomic.fna.gz/') ;
+            wget "$url/$fname" ;
+        done ;
+    done
