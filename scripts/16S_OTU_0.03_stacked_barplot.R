df9928ab376cfdc0e685e642a8a625d855a0e161
Sam Waterworth <60663840+samche42@users.noreply.github.com>
2024-04-17 14:34:00 -0400

    Update 16S_OTU_0.03_stacked_barplot.R

--- scripts/16S_OTU_0.03_stacked_barplot.R
+++ scripts/16S_OTU_0.03_stacked_barplot.R
@@ -24,5 +24,5 @@ ggplot() +
   theme(strip.text.x = element_text(angle = 90,size=5),
         panel.spacing=unit(0,"lines"),
         panel.background = element_blank(),
-        axis.text.x=element_text(angle = 90),
+        axis.text.x=element_text(angle = 90, hjust = 0.95, vjust = 0.2),
         axis.ticks.x=element_blank())
