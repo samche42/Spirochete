fd50e183ba4980457552abf02a4172abbfcf4374
Sam Waterworth <60663840+samche42@users.noreply.github.com>
2024-04-18 10:37:42 -0400

    Create 16S_Chemotype_Indicator_Species_Analysis.R

---
+++ scripts/16S_Chemotype_Indicator_Species_Analysis.R
@@ -0,0 +1,11 @@
+#Taken from https://jkzorz.github.io/2019/07/02/Indicator-species-analysis.html
+
+library(indicspecies)
+data = read.csv("chemotype_0.01_abundance_data.txt", header= TRUE, sep= "\t")
+
+abund = data[,4:ncol(data)] #Collect only numerical data, starts in 4th column
+chemotype = data$Chemotype
+
+inv = multipatt(abund, chemotype, func = "r.g", control = how(nperm=1000),duleg = TRUE) #Run analysis. The duleg parameter prevents group site combinations being assessed
+
+summary(inv)
